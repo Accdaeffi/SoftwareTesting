e17e303904a0b4f0a543517692c852d10f975459
Accdaeffi <noskoff2@yandex.ru>
2020-10-11 13:48:40 +0300

    trying to make graph
    unsuccesfull

--- lab2/src/main/Main.java
+++ lab2/src/main/Main.java
@@ -1,6 +1,14 @@
 package main;
 
 import java.util.Scanner;
+import javax.swing.JFrame;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
 import cos.Cos;
 import ln.Ln;
@@ -11,13 +19,33 @@ import trig.Trig;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		XYSeries series = new XYSeries("function");
+
+	    for(double i = -7; i < -2; i+=0.0001){
+	      series.add(i, finalFunction(i));
+	    }
+
+	    XYDataset xyDataset = new XYSeriesCollection(series);
+	    JFreeChart chart = ChartFactory
+	        .createXYLineChart("function", "x", "y",
+	                           xyDataset, 
+	                           PlotOrientation.VERTICAL,
+	                           true, true, true);
+	    JFrame frame = 
+	        new JFrame("MinimalStaticChart");
+	    // Помещаем график на фрейм
+	    frame.getContentPane()
+	        .add(new ChartPanel(chart));
+	    frame.setSize(400,300);
+	    frame.show();
+		
+		/*Scanner sc = new Scanner(System.in);
 		
 		double x = sc.nextDouble();
 		
 		System.out.println(finalFunction(x));
 		
-		sc.close();
+		sc.close();*/
 	}
 	
 	public static double finalFunction(double x) {
--- lab2/src/tests/TestMain.java
+++ lab2/src/tests/TestMain.java
@@ -13,6 +13,8 @@ class TestMain {
 		assertEquals(Main.finalFunction(-13), 8.32342931334835526139986804652012677689196177648113288829218, 0.00001);
 		assertEquals(Main.finalFunction(-5), -14.71039706840404903345337261621642575199046046272663702102186273, 0.00001);
 		assertEquals(Main.finalFunction(-3), 2296.3104834967403293254003682080696674800769757667293648237646577, 0.00001);
+		//assertEquals(Main.finalFunction(-3.14), 155421083513.02891, 0.00001);
+
 		assertEquals(Main.finalFunction(0), Double.NaN);
 		assertEquals(Main.finalFunction(1), Double.NaN);
 		assertEquals(Main.finalFunction(0.3), 3669.61, 0.1);
